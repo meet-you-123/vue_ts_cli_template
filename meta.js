0bcdebb58b082b66af215eb3e8a0715dae4324aa
杨素桢 <3352439264@qq.com>
2021-11-11 10:19:27 +0800

    修改meta的输出方式

--- meta.js
+++ meta.js
@@ -1,36 +1,36 @@
-module.exports = {
-    prompts: [{
-            name: "name",
-            type: "input",
-            required: true,
-            message: "Project name"
-        },
-        {
-            name: "author",
-            type: "input",
-            message: Author,
-        },
-        {
-            name: "router",
-            type: "confirm",
-            message: "Install vue-router"
-        },
-        {
-            name: "store",
-            type: "confirm",
-            message: "Install vuex"
-        },
-        {
-            name: "vant",
-            type: "confirm",
-            message: "Install vant"
-        }
-    ],
-
-
-    filter: {
-        "src/router/**/*": "router",
-        "src/store/**/*": "store",
-        "src/plugins/**/*": "vant"
+const prompts = [{
+        name: "name",
+        type: "input",
+        required: true,
+        message: "Project name"
+    },
+    {
+        name: "author",
+        type: "input",
+        message: Author,
+    },
+    {
+        name: "router",
+        type: "confirm",
+        message: "Install vue-router"
+    },
+    {
+        name: "store",
+        type: "confirm",
+        message: "Install vuex"
+    },
+    {
+        name: "vant",
+        type: "confirm",
+        message: "Install vant"
     }
+]
+const filter = {
+    "src/router/**/*": "router",
+    "src/store/**/*": "store",
+    "src/plugins/**/*": "vant"
+}
+module.exports = {
+    prompts,
+    filter
 }
